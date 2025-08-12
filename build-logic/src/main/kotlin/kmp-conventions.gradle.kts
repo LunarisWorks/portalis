e1ac8bce5c3f3dbb3bee09db8fbcab9c7d2ca2b9
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-08-12 13:26:25 +0700

    build: set minimum SDK version in Android configuration

--- build-logic/src/main/kotlin/kmp-conventions.gradle.kts
+++ build-logic/src/main/kotlin/kmp-conventions.gradle.kts
@@ -15,6 +15,9 @@ kotlin {
 android {
     compileSdk = 36
     namespace = resolveNamespace()
+    defaultConfig {
+        minSdk = 24
+    }
 }
 
 fun Project.resolveNamespace(): String = "$group.${name.split('-').drop(1).joinToString(".")}"
