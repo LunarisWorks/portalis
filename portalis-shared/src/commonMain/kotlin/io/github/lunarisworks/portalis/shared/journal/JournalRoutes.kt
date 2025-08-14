ebb915f8f7dd381d6ddc1fff9f5b58eb01f126e7
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-08-14 22:33:41 +0700

    fix(shared)!: make journal endpoint plural

--- portalis-shared/src/commonMain/kotlin/io/github/lunarisworks/portalis/shared/journal/JournalRoutes.kt
+++ portalis-shared/src/commonMain/kotlin/io/github/lunarisworks/portalis/shared/journal/JournalRoutes.kt
@@ -1,6 +1,6 @@
 package io.github.lunarisworks.portalis.shared.journal
 
 object JournalRoutes {
-    const val BASE_PATH = "/journal"
+    const val BASE_PATH = "/journals"
     const val ID_PATH = "/{id}"
 }
